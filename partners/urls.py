5245ca2e386502ef454843d10327577538804f2d
Холмухаммад <143716091+Myhammadsak@users.noreply.github.com>
2025-03-02 12:58:45 +0700

    Add files via upload

--- partners/urls.py
+++ partners/urls.py
@@ -6,5 +6,7 @@ app_name = 'partners'
 urlpatterns = [
     path('new/', views.create_partner, name='new_partner'),
     path('', views.partners, name='partners'),
-    path('info/<int:pk>/', views.info_partner, name='info_partner')
+    path('info/<int:pk>/', views.info_partner, name='info_partner'),
+    path('delete/<int:pk>/', views.delete, name='delete'),
+    path('edit/<int:pk>/', views.edit, name='edit_partner')
 ]
--- partners/views.py
+++ partners/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PartnersForm, PartnersImageFormSet
-from .models import Partners
+from .models import Partners, PartnersImage
 
 
 def create_partner(request):
@@ -27,5 +28,33 @@ def partners(request):
 
 def info_partner(request, pk):
     partner = get_object_or_404(Partners, pk=pk)
+    images = PartnersImage.objects.filter(product=partner)
 
-    return render(request, 'partners/detail_partner.html', context={'partner': partner})
+    return render(request, 'partners/detail_partner.html', context={'partner': partner, 'images': images})
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def delete(request, pk):
+    event = get_object_or_404(Partners, pk=pk)
+    event.delete()
+
+    return redirect('/')
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def edit(request, pk):
+    partner = get_object_or_404(Partners, pk=pk)
+    if request.method == 'POST':
+        form = PartnersForm(request.POST, request.FILES, instance=partner)
+        formset = PartnersImageFormSet(request.POST, request.FILES, instance=partner)
+        if form.is_valid() and formset.is_valid():
+            form.save()
+            formset.save()
+
+            return redirect('/')
+
+    else:
+        form = PartnersForm(instance=partner)
+        formset = PartnersImageFormSet(instance=partner)
+
+    return render(request, 'partners/edit_partners.html', context={'form': form, 'formset': formset})
